cff46d5b4d1fac80a39729b6f3de34439a50381d
Brett <madswitchman@yahoo.com>
2020-04-22 10:38:37 -0500

    Simple changes to create dev branch

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -14,7 +14,16 @@ console.log('File written')
 
 // Non-blocking, async way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) =>{
-    console.log(data);
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) =>{
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written');
+            });
+        });
+    });
 });
-console.log('Will read File!')
+console.log('Will read File!');
