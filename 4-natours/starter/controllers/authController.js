59a0461b36cf24c6cb7971207657db43f89a9c27
Brett <madswitchman@yahoo.com>
2020-06-03 11:07:42 -0500

    Prepared app for deploymet

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -40,7 +40,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
   const url = `${req.protocol}://${req.get('host')}/me`;
-  console.log(url);
+  //console.log(url);
   await new Email(newUser, url).sendWelcome();
   // name: req.body.name,
   // email: req.body.email,
--- 4-natours/starter/public/js/stripe.js
+++ 4-natours/starter/public/js/stripe.js
@@ -7,17 +7,15 @@ const stripe = Stripe('pk_test_1jKpp6WDJU8zaOnAdQ86GNNV00NQEt4UjA');
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await Axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await Axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    //console.log(session);
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
   } catch (err) {
-    console.log(err);
+    //console.log(err);
     showAlert('error', err);
   }
 };
